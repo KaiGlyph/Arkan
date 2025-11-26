034450be3ff846750d9a43f202738e21d2804531
Jordi Gallardo Sánchez <jordigallardo0621@gmail.com>
2025-11-26 20:17:34 +0100

    Encontrado error Ventana

--- src/components/HUD.tsx
+++ src/components/HUD.tsx
@@ -90,7 +90,7 @@ export default function HUD() {
           justifyContent: 'center',
         }}>
           <img 
-            src="../src/assets/Arkan-Logo.png" 
+            src="Arkan-Logo.png" 
             alt="Arkan Protocol" 
             style={{ 
               width: '100%', 
